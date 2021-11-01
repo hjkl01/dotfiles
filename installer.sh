5425949784243891383d319b971691060c8e9d7f
The handsome boy <762293490@qq.com>
2021-11-01 10:27:47 +0800

    update install function

--- installer.sh
+++ installer.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
+judgeIfExists(){
+    if [ -e $2 ]; then
+        echo "$2 exists"
+    else
+        echo "run $1 $2"
+        ln -s $1 $2
+    fi
+}
+
+
+SoftLinks(){
+    mkdir -p ~/.config/pip
+    judgeIfExists ~/.dotfiles/config/pip.conf ~/.config/pip/pip.conf
+    judgeIfExists ~/.dotfiles/config/gitconfig ~/.gitconfig
+    judgeIfExists ~/.dotfiles/config/npmrc ~/.npmrc
+    judgeIfExists ~/.dotfiles/config/yarnrc ~/.yarnrc
+}
+
 InstallZsh(){
     echo "installing..."
     if which apt-get >/dev/null; then
-        sudo apt-get update -y && sudo apt-get install -y zsh git python3 python3-venv make neovim npm && curl -sL https://deb.nodesource.com/setup | sudo bash -
+        sudo apt-get update -y && sudo apt-get install -y zsh git python3 python3-venv neovim npm && curl -sL https://deb.nodesource.com/setup | sudo bash -
     elif which brew >/dev/null;then
         brew install zsh git python3 neovim npm trash-cli
     elif which yum >/dev/null;then
@@ -31,12 +49,29 @@ InstallOhMyZsh(){
 }
 
 InstallThemesPlugins(){
-	echo $1
+    echo $1
     git clone $1/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
     git clone $1/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
     ln -s ~/.dotfiles/zsh/Schminitz.zsh-theme ~/.oh-my-zsh/custom/themes/Schminitz.zsh-theme
 }
 
+
+echo $@
+
+# if [ $1 = 'y' ];
+# then
+#     SoftLinks
+# fi
+
+# if [[ "$@" =~ "link"  ]];then
+#     echo "link in args, run soft link"
+#     SoftLinks
+# fi
+
+# 如果可以正常访问GitHub、pip等 注释此行
+SoftLinks
+# install python env
+python3 -m venv ~/.pyenv/py3
 
 InstallZsh
 InstallOhMyZsh $1
