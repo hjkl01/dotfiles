f36709bec2201b6c0398f2ec243a90f38c1fa79a
ycm76229 <ycm76229@gmail.com>
2022-06-29 11:38:20 +0800

    update config

--- zsh/load_yarn.sh
+++ zsh/load_yarn.sh
@@ -2,7 +2,17 @@
 alias yy='yarn'
 alias ys='yarn start'
 
-
 # yrm
 # yarn config set registry https://registry.npm.taobao.org/
 # yarn config get registry
+
+# HUAWEI
+# https://repo.huaweicloud.com/repository/npm/
+# Tencent
+# http://mirrors.cloud.tencent.com/npm/
+# 浙江大学
+# http://mirrors.zju.edu.cn/npm/
+# 南京邮电
+# https://mirrors.njupt.edu.cn/nexus/repository/npm/
+# npmjs
+# https://registry.npmjs.org
