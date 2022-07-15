4c07e045b678d7a30e30b0f083890f04d6c17b90
ycm76229 <ycm76229@gmail.com>
2022-07-15 09:26:55 +0800

    update neovim config

--- installer.sh
+++ installer.sh
@@ -1,68 +1,68 @@
 #!/bin/sh
 
 judgeIfExists(){
-    if [ -e $2 ]; then
-        echo "$2 exists"
-    else
-        echo "run $1 $2"
-        ln -s $1 $2
-    fi
+   if [ -e $2 ]; then
+      echo "$2 exists"
+   else
+      echo "run $1 $2"
+      ln -s $1 $2
+   fi
 }
 
 
 SoftLinks(){
-    mkdir -p ~/.config/pip
-    judgeIfExists ~/.dotfiles/config/pip.conf ~/.config/pip/pip.conf
+   mkdir -p ~/.config/pip
+   judgeIfExists ~/.dotfiles/config/pip.conf ~/.config/pip/pip.conf
 }
 
 InstallZsh(){
-    echo "installing..."
-    if which apt-get >/dev/null; then
-        sudo apt-get update -y && sudo apt-get install -y zsh git python3 python3-venv neovim
-        # npm install -g n
-        # && curl -sL https://deb.nodesource.com/setup | sudo bash -
-    elif which brew >/dev/null;then
-        brew install zsh git python3 neovim trash-cli
-    elif which yum >/dev/null;then
-        sudo yum install zsh git python3 neovim -y
-    elif which pacman >/dev/null;then
-        sudo pacman -Syy && sudo pacman -S --noconfirm zsh git python3 neovim xclip xorg-xclipboard trash-cli
-    else
-        echo "Does not support system version"
-    fi
+   echo "installing..."
+   if which apt-get >/dev/null; then
+      sudo apt-get update -y && sudo apt-get install -y zsh git python3 python3-venv neovim
+      # npm install -g n
+      # && curl -sL https://deb.nodesource.com/setup | sudo bash -
+   elif which brew >/dev/null;then
+      brew install zsh git python3 neovim trash-cli
+   elif which yum >/dev/null;then
+      sudo yum install zsh git python3 neovim -y
+   elif which pacman >/dev/null;then
+      sudo pacman -Syy && sudo pacman -S --noconfirm zsh git python3 neovim xclip xorg-xclipboard trash-cli
+   else
+      echo "Does not support system version"
+   fi
 }
 
 InstallOhMyZsh(){
-    if [ -e ~/.oh-my-zsh ]; then
-        echo "~/.oh-my-zsh exists"
-    else
-        git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-    fi
+   if [ -e ~/.oh-my-zsh ]; then
+      echo "~/.oh-my-zsh exists"
+   else
+      git clone --depth 1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+   fi
 
-    if [ -e ~/.zshrc ];then
-        echo "~/.zshrc exists"
-    else
-        ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-        chsh -s $(which zsh)
-    fi
+   if [ -e ~/.zshrc ];then
+      echo "~/.zshrc exists"
+   else
+      ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+      chsh -s $(which zsh)
+   fi
 }
 
 InstallThemesPlugins(){
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-    ln -s ~/.dotfiles/zsh/Schminitz.zsh-theme ~/.oh-my-zsh/custom/themes/Schminitz.zsh-theme
+   git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+   git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+   ln -s ~/.dotfiles/zsh/Schminitz.zsh-theme ~/.oh-my-zsh/custom/themes/Schminitz.zsh-theme
 }
 
 InstallNeovim(){
-    git clone https://github.com/NvChad/NvChad init.lua
-    ln -s ~/.dotfiles/nvim_custom ~/.dotfiles/init.lua/lua/custom
-    ln -s ~/.dotfiles/init.lua ~/.config/nvim
-    ln -s ~/.dotfiles/config/pycodestyle ~/.config
-    # git clone https://github.com/hjkl01/init.lua nvim
-    # ln -s ~/.dotfiles/nvim ~/.config
-    # ln -s ~/.dotfiles/nvim/pycodestyle ~/.config
-    ~/.venv/py3/bin/pip install better_exceptions neovim black -i https://pypi.tuna.tsinghua.edu.cn/simple
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+   git clone --depth 1 https://github.com/NvChad/NvChad init.lua
+   ln -s ~/.dotfiles/nvim_custom ~/.dotfiles/init.lua/lua/custom
+   ln -s ~/.dotfiles/init.lua ~/.config/nvim
+   ln -s ~/.dotfiles/config/pycodestyle ~/.config
+   # git clone --depth 1 https://github.com/hjkl01/init.lua nvim
+   # ln -s ~/.dotfiles/nvim ~/.config
+   # ln -s ~/.dotfiles/nvim/pycodestyle ~/.config
+   ~/.venv/py3/bin/pip install better_exceptions neovim black -i https://pypi.tuna.tsinghua.edu.cn/simple
+   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 }
 
 
--- nvim_custom/chadrc.lua
+++ nvim_custom/chadrc.lua
@@ -21,15 +21,15 @@ M.plugins = {
 }
 
 M.ui = {
-    hl_override = {
-    },
-    changed_themes = {
-        onedark = {},
-    },
-    theme_toggle = { "onedark", "one_light" },
-    -- theme_toggle = { "onedark", "dark" },
-    theme = "onedark", -- default theme
-    transparency = false,
+   hl_override = {
+   },
+   changed_themes = {
+      onedark = {},
+   },
+   theme_toggle = { "onedark", "one_light" },
+   -- theme_toggle = { "onedark", "dark" },
+   theme = "onedark", -- default theme
+   transparency = false,
 }
 
 M.mappings = require "custom.mappings"
@@ -37,31 +37,14 @@ M.mappings = require "custom.mappings"
 M.options = {
    user = function()
       local opt = vim.opt
-      local g = vim.g
-
-      -- use filetype.lua instead of filetype.vim
-      g.did_load_filetypes = 0
-      g.do_filetype_lua = 1
-      g.toggle_theme_icon = "   "
+      -- local g = vim.g
 
       opt.confirm = true
-      opt.laststatus = 3 -- global statusline
-      opt.title = true
-      opt.clipboard = "unnamedplus"
       opt.cmdheight = 1
-      opt.cul = true -- cursor line
-
-      -- Indentline
-      opt.expandtab = true
-      opt.shiftwidth = 4
-      opt.smartindent = true
 
       -- 折叠
       opt.foldenable = false
       opt.foldmethod = "indent"
-
-      -- disable tilde on end of buffer: https://github.com/neovim/neovim/pull/8546#issuecomment-643643758
-      opt.fillchars = { eob = " " }
 
       -- 补全增强
       opt.wildmenu = true
@@ -70,34 +53,12 @@ M.options = {
       -- 边输入边搜索
       opt.incsearch = true
       opt.hidden = true
-      opt.ignorecase = true
-      opt.smartcase = true
-      opt.mouse = "a"
 
       -- Numbers
       opt.number = true
       opt.numberwidth = 2
       opt.relativenumber = true
       opt.ruler = false
-
-      -- disable nvim intro
-      opt.shortmess:append "sI"
-
-      opt.signcolumn = "yes"
-      opt.splitbelow = true
-      opt.splitright = true
-      opt.tabstop = 4
-      opt.termguicolors = true
-      opt.timeoutlen = 400
-      opt.undofile = true
-
-      -- interval for writing swap file to disk, also used by gitsigns
-      opt.updatetime = 250
-
-      -- go to previous/next line with h,l,left arrow and right arrow
-      -- when cursor reaches end/beginning of line
-      opt.whichwrap:append "<>[]hl"
-      g.mapleader = " "
 
    end,
 }
