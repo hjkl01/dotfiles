cf445be3bda007b1cfb419347c0a04953c565d4a
ycm76229 <ycm76229@gmail.com>
2022-08-01 17:52:11 +0800

    update podman config

--- zsh/load_docker.sh
+++ zsh/load_docker.sh
@@ -6,13 +6,19 @@
 # https://hub-mirror.c.163.com
 # https://mirror.baidubce.com
 
-alias dp='docker ps -a'
-# stop all containers 谨慎使用
-alias dk='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-# delete null images
-alias dr='docker rmi $(docker images --filter dangling=true -q --no-trunc)'
-# delete exited containers
-alias dre='docker rm -v $(docker ps -a -q -f status=exited)'
-alias dc='docker-compose'
-alias di='docker images'
-alias dir='docker rmi'
+# alias dp='docker ps -a'
+# # stop all containers 谨慎使用
+# alias dk='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+# # delete null images
+# alias dr='docker rmi $(docker images --filter dangling=true -q --no-trunc)'
+# # delete exited containers
+# alias dre='docker rm -v $(docker ps -a -q -f status=exited)'
+# alias dc='docker-compose'
+# alias di='docker images'
+# alias dir='docker rmi'
+
+
+# podman config
+alias dp='podman ps -a'
+alias di='podman images'
+alias dc='podman-compose'
