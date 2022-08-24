27bc24dd01983cd7a72cddae9b89980272ddb8e7
ycm76229 <ycm76229@gmail.com>
2022-08-24 15:41:09 +0800

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -18,9 +18,8 @@ beforeInstall(){
 }
 
 linkFile(){
-   if [ -e $2 ]; then
+  if [ -e $2 ]; then
       echo "$2 exists"
-      exit
    else
       echo "run $1 $2"
       ln -s $1 $2
@@ -59,21 +58,21 @@ InstallOhMyZsh(){
 InstallThemesPlugins(){
    git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
    git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-   linkFile -s ~/.dotfiles/zsh/Schminitz.zsh-theme ~/.oh-my-zsh/custom/themes/Schminitz.zsh-theme
+   linkFile ~/.dotfiles/zsh/Schminitz.zsh-theme ~/.oh-my-zsh/custom/themes/Schminitz.zsh-theme
 }
 
 InstallNeovim(){
    # git clone --depth 1 https://github.com/hjkl01/init.lua init.lua
    # linkFile -s ~/.dotfiles/init.lua ~/.config/nvim
    git clone --depth 1 https://github.com/NvChad/NvChad  init.lua
-   linkFile -s ~/.dotfiles/init.lua ~/.config/nvim
-   linkFile -s ~/.dotfiles/config/pycodestyle ~/.config
+   linkFile ~/.dotfiles/custom ~/.dotfiles/init.lua/lua/custom
+   linkFile ~/.dotfiles/init.lua ~/.config/nvim
+   linkFile ~/.dotfiles/config/pycodestyle ~/.config
 
    # install python env
    python3 -m venv ~/.venv/py3
    ~/.venv/py3/bin/pip install better_exceptions neovim black -i https://pypi.tuna.tsinghua.edu.cn/simple
    # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-   nvim --headless -c 'PackerSync'
 }
 
 
