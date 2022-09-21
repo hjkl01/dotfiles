8791e3b6b5b9904d652094bead08540eac6410c5
ycm76229 <ycm76229@gmail.com>
2022-09-21 17:11:52 +0800

    update keymap

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -386,4 +386,60 @@ M.blankline = {
   },
 }
 
+M.translate = {
+  n = {
+    ["tt"] = { "<Plug>TranslateW", "TranslateW" },
+  },
+  v = {
+    ["tt"] = { "<Plug>TranslateWV", "TranslateWV" },
+  },
+}
+
+M.others = {
+  n = {
+    ["ff"] = {
+      function()
+        -- echo &filetype
+        vim.cmd [[
+                    exec "w"
+                    if &filetype == 'python'
+                        exec "r !black -l 120 -q %"
+                        " exec "r !yapf -i %"
+                        exec "e"
+                    elseif &filetype == 'json'
+                        exec "%!python3 -c 'import json, sys, collections; print(json.dumps(json.load(sys.stdin, object_pairs_hook=collections.OrderedDict), ensure_ascii=False, indent=4))'"
+                    else
+                        exec ":Neoformat <CR>"
+                        exec "w"
+                    endif
+                ]]
+      end,
+      "autoformat",
+    },
+
+    [" r"] = {
+      function()
+        vim.cmd [[ 
+                    exec "w"
+                    if &filetype == 'python'
+                        "exec "!time python %"
+                        exec ':bo 20sp | terminal python %'
+                    elseif &filetype == 'go'
+                        exec ':bo 10sp | terminal go run %'
+                    elseif &filetype == 'sh'
+                        exec ':bo 10sp | terminal sh %'
+                    elseif &filetype == 'javascript'
+                        exec ':bo 10sp | terminal node %'
+                    elseif &filetype == 'markdown'
+                        exec ':MarkdownPreview '
+                    else
+                        echo &filetype
+                    endif
+                ]]
+      end,
+      "run file",
+    },
+  },
+}
+
 return M
