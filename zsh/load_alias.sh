7b088db048dcf3601ad3876d8ee0d228fbfbc84a
ycm76229 <ycm76229@gmail.com>
2022-12-14 14:53:10 +0800

    add mac set proxy command

--- zsh/load_alias.sh
+++ zsh/load_alias.sh
@@ -6,16 +6,16 @@
 #   # if type nvim > /dev/null 2>&1; then
 #   #   alias vi='nvim'
 #   # fi
-# 
+#
 #   # fail on non-zero return value
 #   if [ "$ret" -ne 0 ]; then
 #     return 1
 #   fi
-# 
+#
 #   alias $2=$1
 #   return 0
 # }
-# 
+#
 # alias_if_program_exists trash-put rr
 # alias_if_program_exists nvim vi
 alias rr="trash-put"
@@ -38,6 +38,11 @@ alias wqr='python ~/.dotfiles/config/sharefile.py $1'
 alias gip='curl -s ipinfo.io'
 alias gip6='curl -6 https://ifconfig.co/ip'
 alias wt='curl wttr.in/nanjing'
+
+# Mac proxy
+alias macproxysetup='networksetup -setwebproxy Wi-Fi 127.0.0.1 8888 && networksetup -setsecurewebproxy Wi-Fi 127.0.0.1 8888 && networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 8888'
+alias macproxystart='networksetup -setwebproxystate Wi-Fi on && networksetup -setsecurewebproxystate Wi-Fi on && networksetup -setsocksfirewallproxystate Wi-Fi on'
+alias macproxystop='networksetup -setwebproxystate Wi-Fi off && networksetup -setsecurewebproxystate Wi-Fi off && networksetup -setsocksfirewallproxystate Wi-Fi off'
 
 # vultr
 export VULTR_API_KEY=''
