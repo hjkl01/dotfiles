5ca898c21824ad984f64372e47f0cd445f5ddb13
ycm76229 <ycm76229@gmail.com>
2023-02-09 18:00:30 +0800

    update nvimtree config

--- nvim/lua/plugins/configs/nvimtree.lua
+++ nvim/lua/plugins/configs/nvimtree.lua
@@ -32,13 +32,7 @@ local options = {
     dotfiles = false,
     exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
   },
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = { "alpha" },
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  update_cwd = true,
+
   update_focused_file = {
     enable = true,
     update_cwd = false,
@@ -73,13 +67,6 @@ local options = {
     },
   },
   renderer = {
-    highlight_git = false,
-    highlight_opened_files = "none",
-
-    indent_markers = {
-      enable = false,
-    },
-
     icons = {
       webdev_colors = true,
       git_placement = "before",
@@ -116,39 +103,6 @@ local options = {
         },
       },
     },
-
-    -- icons = {
-    --   show = {
-    --     file = true,
-    --     folder = true,
-    --     folder_arrow = true,
-    --     git = false,
-    --   },
-    --
-    --   glyphs = {
-    --     default = "",
-    --     symlink = "",
-    --     folder = {
-    --       default = "",
-    --       empty = "",
-    --       empty_open = "",
-    --       open = "",
-    --       symlink = "",
-    --       symlink_open = "",
-    --       arrow_open = "",
-    --       arrow_closed = "",
-    --     },
-    --     git = {
-    --       unstaged = "✗",
-    --       staged = "✓",
-    --       unmerged = "",
-    --       renamed = "➜",
-    --       untracked = "★",
-    --       deleted = "",
-    --       ignored = "◌",
-    --     },
-    --   },
-    -- },
   },
 }
 
