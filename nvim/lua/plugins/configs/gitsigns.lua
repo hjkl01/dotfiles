a20a54c88cf85dbe70dd498a1a79c0d026422375
ycm76229 <ycm76229@gmail.com>
2023-02-27 10:34:18 +0800

    update Gitsigns blame config

--- nvim/lua/plugins/configs/gitsigns.lua
+++ nvim/lua/plugins/configs/gitsigns.lua
@@ -22,7 +22,7 @@ local options = {
     follow_files = true,
   },
   attach_to_untracked = true,
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -47,6 +47,4 @@ local options = {
   },
 }
 
-local load_override = require("core.utils").load_override
-options = load_override(options, "lewis6991/gitsigns.nvim")
 gitsigns.setup(options)
