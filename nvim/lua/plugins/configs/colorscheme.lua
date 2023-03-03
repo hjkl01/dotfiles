3aeee427b9306a082f71148b8eb40d3177cacea2
ycm76229 <ycm76229@gmail.com>
2023-03-03 18:32:37 +0800

    update colorscheme config

--- nvim/lua/plugins/configs/colorscheme.lua
+++ nvim/lua/plugins/configs/colorscheme.lua
@@ -41,7 +41,7 @@ local options = {
     -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = { italic = true },
     keywords = { italic = true },
-    functions = {},
+    functions = { italic = true },
     variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
     sidebars = "dark", -- style for sidebars, see below
@@ -55,15 +55,14 @@ local options = {
   --- You can override specific color groups to use other groups or a hex color
   --- function will be called with a ColorScheme table
   ---@param colors ColorScheme
-  on_colors = function(colors)
-  end,
+  on_colors = function(colors) end,
   --- You can override specific highlights to use other groups or a hex color
   --- function will be called with a Highlights and ColorScheme table
   --- @param highlights Highlights
   --- @param colors ColorScheme
   on_highlights = function(highlights, colors)
     highlights.LineNr = {
-      bg = '#000000'
+      bg = "#000000",
     }
   end,
   -- },
