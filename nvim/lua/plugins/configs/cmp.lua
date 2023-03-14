98fb0b94176ac482324860fe4010577262942c94
ycm76229 <ycm76229@gmail.com>
2023-03-14 22:59:00 +0800

    update cmp

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -17,12 +17,16 @@ local options = {
   completion = {
     completeopt = "menu,menuone",
   },
-
+  view = {
+    -- entries = "custom", -- can be "custom", "wildmenu" or "native"
+    entries = { name = "custom", selection_order = "near_cursor" },
+  },
   window = {
     completion = {
-      side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+      -- side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+      side_padding = 1,
       winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
-      scrollbar = false,
+      scrollbar = true,
     },
     documentation = {
       border = border "CmpDocBorder",
@@ -34,10 +38,39 @@ local options = {
       require("luasnip").lsp_expand(args.body)
     end,
   },
-
-  -- formatting = formatting_style,
-  formatting = {},
-
+  formatting = {
+    PmenuSel = { bg = "#282C34", fg = "NONE" },
+    Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
+    CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", fmt = "strikethrough" },
+    CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE", fmt = "bold" },
+    CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE", fmt = "bold" },
+    CmpItemMenu = { fg = "#C792EA", bg = "NONE", fmt = "italic" },
+    CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+    CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+    CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+    CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+    CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+    CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+    CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+    CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+    CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+    CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+    CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+    CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+    CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+    CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+    CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+    CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+    CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+    CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+    CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+    CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+  },
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
@@ -82,9 +115,5 @@ local options = {
     { name = "path" },
   },
 }
-
-if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
-  options.window.completion.border = border "CmpBorder"
-end
 
 return options
