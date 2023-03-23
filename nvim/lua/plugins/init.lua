aa1052fb069e3006042f12592d14edd5010b78bb
ycm76229 <ycm76229@gmail.com>
2023-03-23 17:45:48 +0800

    update translate plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -204,12 +204,26 @@ local default_plugins = {
     end,
   },
   {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    setup = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+    "JuanZoran/Trans.nvim",
+    -- run = 'bash ./install.sh',
+    -- https://github.com/skywind3000/ECDICT-ultimate/releases/download/1.0.0/ecdict-ultimate-sqlite.zip
+    keys = {
+      -- 可以换成其他你想映射的键
+      { 'tt', mode = { 'n', 'x' }, '<Cmd>Translate<CR>', desc = ' Translate' },
+      { 'ts', mode = { 'n', 'x' }, '<Cmd>TransPlay<CR>', desc = ' 自动发音' },
+
+      -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
+      -- { 'mi', '<Cmd>TranslateInput<CR>', desc = ' Translate From Input' },
+    },
+    dependencies = { 'kkharji/sqlite.lua', lazy = true, },
+    opts = {
+      -- your configuration there
+    },
+    config = function()
+      require("Trans").setup {
+        db_path = '$HOME/.dotfiles/nvim/ultimate.db',
+      }
+    end
   },
 }
 
