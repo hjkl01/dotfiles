84d5d522cd108279ccf6037be19b04e5b5788239
ycm76229 <ycm76229@gmail.com>
2023-06-01 11:33:01 +0800

    delete useless icons

--- nvim/lua/plugins/configs/mason.lua
+++ nvim/lua/plugins/configs/mason.lua
@@ -12,23 +12,26 @@ local options = {
     "typescript-language-server",
   }, -- not an option from mason.nvim
   PATH = "skip",
-  ui = {
-    icons = {
-      package_pending = " ",
-      package_installed = " ",
-      package_uninstalled = " ﮊ",
-    },
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
-      cancel_installation = "<C-c>",
-    },
-  },
+  -- ui = {
+  --   icons = {
+  --     package_installed = "✓",
+  --     package_pending = "➜",
+  --     package_uninstalled = "✗",
+  --     -- package_pending = " ",
+  --     -- package_installed = " ",
+  --     -- package_uninstalled = " ﮊ",
+  --   },
+  --   -- keymaps = {
+  --   --   toggle_server_expand = "<CR>",
+  --   --   install_server = "i",
+  --   --   update_server = "u",
+  --   --   check_server_version = "c",
+  --   --   update_all_servers = "U",
+  --   --   check_outdated_servers = "C",
+  --   --   uninstall_server = "X",
+  --   --   cancel_installation = "<C-c>",
+  --   -- },
+  -- },
   max_concurrent_installers = 10,
 }
 
