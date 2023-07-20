007b035877871c66caf98b92ca1c34548355e08c
ycm76229 <ycm76229@gmail.com>
2023-07-20 15:18:46 +0800

    update nvimtree config

--- nvim/lua/plugins/configs/nvimtree.lua
+++ nvim/lua/plugins/configs/nvimtree.lua
@@ -273,22 +273,17 @@ require("nvim-tree").setup { -- BEGIN_DEFAULT_OPTS
 
 -- nvim-tree 自动关闭
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
-vim.api.nvim_create_autocmd({ "QuitPre" }, {
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+  pattern = "NvimTree_*",
   callback = function()
-    vim.cmd "NvimTreeClose"
+    local layout = vim.api.nvim_call_function("winlayout", {})
+    if
+        layout[1] == "leaf"
+        and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+        and layout[3] == nil
+    then
+      vim.cmd "confirm quit"
+    end
   end,
 })
--- autocmd("BufEnter", {
---   group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
---   pattern = "NvimTree_*",
---   callback = function()
---     local layout = vim.api.nvim_call_function("winlayout", {})
---     if
---         layout[1] == "leaf"
---         and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
---         and layout[3] == nil
---     then
---       vim.cmd "confirm quit"
---     end
---   end,
--- })
