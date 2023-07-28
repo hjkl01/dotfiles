9648d794826ed26e59b64b62e9ac10dd1509e859
ycm76229 <ycm76229@gmail.com>
2023-07-28 23:53:12 +0800

    delete noice

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -70,26 +70,6 @@ local ui_plugins = {
       require("which-key").setup(opts)
     end,
   },
-
-  -- lazy.nvim
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require "plugins.configs.noice"
-    end,
-  },
 }
 
 return ui_plugins
