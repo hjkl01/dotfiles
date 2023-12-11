0a7be75035860f12541ed7d097b3a436cb80bbc1
ycm76229 <ycm76229@gmail.com>
2023-12-11 16:30:08 +0800

    update fzf

--- zsh/load_docker.sh
+++ zsh/load_docker.sh
@@ -8,18 +8,31 @@
 # https://hub-mirror.c.163.com
 # https://mirror.baidubce.com
 
-alias dp='docker ps -a'
+alias dop='docker ps -a'
 # stop all containers 谨慎使用
-alias dkkkk='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+# alias doka='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 # delete null images
-alias dr='docker rmi $(docker images --filter dangling=true -q --no-trunc)'
+# alias dr='docker rmi $(docker images --filter dangling=true -q --no-trunc)'
 # delete exited containers
-alias dre='docker rm -v $(docker ps -a -q -f status=exited)'
+# alias dre='docker rm -v $(docker ps -a -q -f status=exited)'
+# clean null images and exited containers
+alias doclean='docker rmi $(docker images --filter dangling=true -q --no-trunc) && docker rm -v $(docker ps -a -q -f status=exited)'
+
 alias dc='docker-compose'
-alias di='docker images'
-alias dir='docker rmi'
+alias doi='docker images'
 
 # podman config
 # alias dp='podman ps -a'
 # alias di='podman images'
 # alias dc='podman-compose'
+
+# Select a docker container to stop and rm
+dosm() {
+	docker ps -a | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $1 }' | xargs -r docker stop | xargs -r docker rm
+}
+
+# Select a docker image or images to remove
+dori() {
+	docker images | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $3 }' | xargs -r docker rmi
+
+}
--- zsh/load_git.sh
+++ zsh/load_git.sh
@@ -16,3 +16,15 @@ gitpush() {
 	git commit -m $1
 	git push
 }
+
+# fshow - git commit browser
+gitcb() {
+	git log --graph --color=always \
+		--format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+		fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+			--bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+FZF-EOF"
+}
