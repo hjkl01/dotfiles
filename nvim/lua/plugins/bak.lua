4097c24a97e33d0f825816ebd77edd8c00ffcdc0
ycm76229 <ycm76229@gmail.com>
2024-01-25 17:34:03 +0800

    update backup markdown

--- nvim/lua/plugins/bak.lua
+++ nvim/lua/plugins/bak.lua
@@ -144,3 +144,14 @@
 --     }
 --   end,
 -- }
+--
+--
+-- {
+--   "iamcco/markdown-preview.nvim",
+--   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--   build = "cd app && yarn install",
+--   init = function()
+--     vim.g.mkdp_filetypes = { "markdown" }
+--   end,
+--   ft = { "markdown" },
+-- },
