4e07fee26cd53a011f0141bfc44cfd9b0bf27325
ycm76229 <ycm76229@gmail.com>
2024-06-08 21:16:13 +0800

    update go mirror

--- zsh/load_go.sh
+++ zsh/load_go.sh
@@ -6,13 +6,13 @@
 # https://studygolang.com/dl
 # Aliyun
 # https://mirrors.aliyun.com/golang/
+# export GOPROXY=https://mirrors.aliyun.com/goproxy/
 # Proxy-io
 # https://gomirrors.org/
 # 中科大
 # https://mirrors.ustc.edu.cn/golang/
 
-export GOPROXY=https://mirrors.aliyun.com/goproxy/
-# export GOPROXY=https://goproxy.cn
+export GOPROXY=https://goproxy.cn
 export GO111MODULE=auto
 export GOPATH=$HOME/dev/go
 export PATH="$PATH:$HOME/dev/go/bin"
