ba411a6816f0f9eabe5a8aa706f4cf982f0da657
ycm76229 <ycm76229@gmail.com>
2024-09-03 14:09:36 +0800

    update docker alias

--- zsh/load_docker.sh
+++ zsh/load_docker.sh
@@ -8,18 +8,18 @@
 # https://hub-mirror.c.163.com
 # https://mirror.baidubce.com
 
-alias dcp='docker ps -a'
+alias dops='docker ps -a'
 # stop all containers 谨慎使用
 # alias dcka='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 # delete null images
-# alias dr='docker rmi $(docker images --filter dangling=true -q --no-trunc)'
+alias dorin='docker rmi $(docker images --filter dangling=true -q --no-trunc)'
 # delete exited containers
-# alias dre='docker rm -v $(docker ps -a -q -f status=exited)'
+alias dorce='docker rm -v $(docker ps -a -q -f status=exited)'
 # clean null images and exited containers
-alias dclean='docker rmi -f $(docker images --filter dangling=true -q --no-trunc) && docker rm -v $(docker ps -a -q -f status=exited)'
+# alias dclean='docker rmi -f $(docker images --filter dangling=true -q --no-trunc) && docker rm -v $(docker ps -a -q -f status=exited)'
 
 alias dc='docker-compose'
-alias dci='docker images'
+alias doi='docker images'
 
 # podman config
 # alias dp='podman ps -a'
@@ -27,12 +27,12 @@ alias dci='docker images'
 # alias dc='podman-compose'
 
 # Select a docker container to stop and rm
-dcps() {
+dosr() {
 	docker ps -a | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $1 }' | xargs -r docker stop | xargs -r docker rm
 }
 
 # Select a docker image or images to remove
-dcir() {
+dori() {
 	docker images | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $3 }' | xargs -r docker rmi
 
 }
