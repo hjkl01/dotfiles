0e5951d5e94285b019ce155e765d307ab3ff37dd
ycm76229 <ycm76229@gmail.com>
2024-12-09 16:39:01 +0800

    update keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("n", "s")
+vim.keymap.del({ "n", "v" }, "s")
 
 local map = vim.keymap.set
 
