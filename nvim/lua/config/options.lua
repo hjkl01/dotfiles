03b7dab2209c23f10e925ec7a26e95b6325e1ac0
ycm76229 <ycm76229@gmail.com>
2024-12-26 15:33:46 +0800

    update add path env

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -65,3 +65,7 @@ opt.incsearch = true
 opt.hidden = true
 opt.ignorecase = true
 opt.smartcase = true
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
