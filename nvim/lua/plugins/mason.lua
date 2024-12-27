a27a719c6768dff0bb5127d4352132374beec106
ycm76229 <ycm76229@gmail.com>
2024-12-27 23:07:55 +0800

    update mason lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,14 +1,14 @@
 return {
   {
     "williamboman/mason.nvim",
-    -- dependencies = { "williamboman/mason-lspconfig.nvim" },
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
     cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
     build = "MasonInstallAll",
     opts = {
       -- install_root_dir = path.concat { vim.fn.stdpath "data", "mason" },
-      PATH = "skip",
+      -- PATH = "skip",
       -- PATH = "append",
-      -- PATH = "prepend",
+      PATH = "prepend",
       max_concurrent_installers = 10,
       github = {
         download_url_template = "https://gh.hjkl01.cn/proxy/https://github.com/%s/releases/download/%s/%s",
@@ -33,21 +33,31 @@ return {
         "typescript-language-server",
         "gopls",
         "sqlfluff",
-      }, -- not an option from mason.nvim
+      },
     },
 
     config = function(_, opts)
       require("mason").setup(opts)
-      -- require("mason-lspconfig").setup()
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "bashls",
+          "gopls",
+          -- 'jsonls',
+          "lua_ls",
+          "ruff",
+          "pylsp",
+          -- "sqlls",
+          "ts_ls",
+          -- "yamlls",
+        },
+        automatic_installation = true,
+      })
 
       -- custom cmd to install all mason binaries listed
       vim.api.nvim_create_user_command("MasonInstallAll", function()
         vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
       end, {})
-
-      -- -- add binaries installed by mason.nvim to path
-      -- local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
-      -- vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
     end,
   }
 }
