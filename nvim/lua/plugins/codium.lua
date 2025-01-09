db515da25b90972f16524141bb92a8476d5d38ba
ycm76229 <ycm76229@gmail.com>
2025-01-09 19:00:29 +0800

    update delete codeium default

--- nvim/lua/plugins/codium.lua
+++ nvim/lua/plugins/codium.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    lazy = false,
-    cmd = "Codeium",
-    event = "InsertEnter",
-    -- build = ":Codeium Auth",
-    opts = {
-      enable_cmp_source = vim.g.ai_cmp,
-      virtual_text = {
-        enabled = not vim.g.ai_cmp,
-        key_bindings = {
-          accept = false, -- handled by nvim-cmp / blink.cmp
-          next = "<M-]>",
-          prev = "<M-[>",
-        },
-      },
-    },
-  }
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   lazy = false,
+  --   cmd = "Codeium",
+  --   event = "InsertEnter",
+  --   -- build = ":Codeium Auth",
+  --   opts = {
+  --     enable_cmp_source = vim.g.ai_cmp,
+  --     virtual_text = {
+  --       enabled = not vim.g.ai_cmp,
+  --       key_bindings = {
+  --         accept = false, -- handled by nvim-cmp / blink.cmp
+  --         next = "<M-]>",
+  --         prev = "<M-[>",
+  --       },
+  --     },
+  --   },
+  -- }
 }
