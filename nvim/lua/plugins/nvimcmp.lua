5dcbdbbbd551b8af1515fe8911039c190769b532
ycm76229 <ycm76229@gmail.com>
2025-04-01 14:27:57 +0800

    update nvimcmp keymap Tab

--- nvim/lua/plugins/nvimcmp.lua
+++ nvim/lua/plugins/nvimcmp.lua
@@ -112,8 +112,6 @@ return {
           CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
         },
         mapping = {
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
           ["<C-u>"] = cmp.mapping.scroll_docs(-4),
           ["<C-d>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
