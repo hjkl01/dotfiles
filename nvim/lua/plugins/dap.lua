1ff4a2341c2ea268017ba6bef868d1aa091825ff
ycm76229 <ycm76229@gmail.com>
2025-06-12 17:47:26 +0800

    update dap keymap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -21,12 +21,12 @@ return {
 
       -- 快捷键绑定
       local map = vim.keymap.set
-      map("n", "<leader>bs", function() dap.continue() end, { desc = "启动调试" })
+      map("n", "<leader>dd", function() dap.continue() end, { desc = "启动调试" })
       map("n", "<F10>", function() dap.step_over() end, { desc = "跳过" })
       map("n", "<F11>", function() dap.step_into() end, { desc = "进入函数" })
       map("n", "<F12>", function() dap.step_out() end, { desc = "跳出函数" })
-      map("n", "<leader>bb", function() dap.toggle_breakpoint() end, { desc = "切换断点" })
-      map("n", "<leader>B", function()
+      map("n", "<leader>ds", function() dap.toggle_breakpoint() end, { desc = "切换断点" })
+      map("n", "<leader>dc", function()
         dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
       end, { desc = "设置条件断点" })
       map("n", "<leader>du", function() dapui.toggle() end, { desc = "切换 dap-ui 面板" })
