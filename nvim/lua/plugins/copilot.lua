52b4aa9b937a16587f3eb9fb8bd7cbd4cdc49378
ycm76229 <ycm76229@gmail.com>
2025-06-13 22:18:14 +0800

    update copilot keymap

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     config = function()
       -- vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
+      vim.api.nvim_set_keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
       -- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
       -- vim.api.nvim_set_keymap("i", "<C-]>", 'copilot#Next()', { silent = true, expr = true, script = true })
       -- vim.api.nvim_set_keymap("i", "<C-[>", 'copilot#Previous()', { silent = true, expr = true, script = true })
