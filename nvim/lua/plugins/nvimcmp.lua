d1a9999609aad7411f1bd3e40fb23e15be459b99
ycm76229 <ycm76229@gmail.com>
2025-07-30 11:37:06 +0800

    update cmp: add buffer

--- nvim/lua/plugins/nvimcmp.lua
+++ nvim/lua/plugins/nvimcmp.lua
@@ -32,6 +32,7 @@ return {
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
       "Saghen/blink.cmp",
 
       -- UI/Icons
@@ -107,6 +108,7 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
+          { name = "buffer" },
           { name = "blink" },
           { name = "nvim_lua" },
           { name = "path" },
