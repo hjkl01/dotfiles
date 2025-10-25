33144fbb8fe96d32b43da33b113353c67927193a
ycm76229 <ycm76229@gmail.com>
2025-10-25 23:10:58 +0800

    auto highlight same word

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Highlight word under cursor
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    if vim.g.current_match_id then
+      vim.fn.matchdelete(vim.g.current_match_id)
+      vim.g.current_match_id = nil
+    end
+    local word = vim.fn.expand("<cword>")
+    if word ~= "" and #word > 1 then
+      vim.g.current_match_id = vim.fn.matchadd("Search", "\\<" .. vim.fn.escape(word, "\\") .. "\\>")
+    end
+  end,
+})
