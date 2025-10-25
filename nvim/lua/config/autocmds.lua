d850afb40af561c344b856ac1970ca543e45a371
ycm76229 <ycm76229@gmail.com>
2025-10-26 00:52:29 +0800

    highlight same word

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,16 +7,19 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Highlight word under cursor
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+
+-- 自动高亮光标下的单词
+vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+  pattern = "*",
   callback = function()
-    if vim.g.current_match_id then
-      vim.fn.matchdelete(vim.g.current_match_id)
-      vim.g.current_match_id = nil
-    end
-    local word = vim.fn.expand("<cword>")
-    if word ~= "" and #word > 1 then
-      vim.g.current_match_id = vim.fn.matchadd("Search", "\\<" .. vim.fn.escape(word, "\\") .. "\\>")
-    end
+    -- 获取光标下的单词
+    local word = vim.fn.expand('<cword>')
+    if not word or word == '' then return end
+
+    -- 清除之前的匹配
+    vim.fn.clearmatches()
+
+    -- 添加高亮（使用 Search 组，你也可以用 Visual 自定义高亮组）
+    vim.fn.matchadd('Search', '\\<' .. vim.fn.escape(word, '\\/') .. '\\>')
   end,
 })
