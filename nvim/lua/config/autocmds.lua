8a15232532cad3dfc750f6356c07e486657baab9
ycm76229 <ycm76229@gmail.com>
2025-12-02 18:00:05 +0800

    update neovim config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,19 +7,31 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-
--- 自动高亮光标下的单词
+-- 自动高亮光标下的单词 (优化性能)
+local highlight_timer = nil
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   pattern = "*",
   callback = function()
-    -- 获取光标下的单词
-    local word = vim.fn.expand('<cword>')
-    if not word or word == '' then return end
+    if highlight_timer then
+      vim.loop.timer_stop(highlight_timer)
+    end
+    highlight_timer = vim.loop.new_timer()
+    highlight_timer:start(
+      200,
+      0,
+      vim.schedule_wrap(function()
+        -- 获取光标下的单词
+        local word = vim.fn.expand("<cword>")
+        if not word or word == "" or #word < 3 then
+          return
+        end
 
-    -- 清除之前的匹配
-    vim.fn.clearmatches()
+        -- 清除之前的匹配
+        vim.fn.clearmatches()
 
-    -- 添加高亮（使用 Search 组，你也可以用 Visual 自定义高亮组）
-    vim.fn.matchadd('Search', '\\<' .. vim.fn.escape(word, '\\/') .. '\\>')
+        -- 添加高亮（使用 Search 组）
+        vim.fn.matchadd("Search", "\\<" .. vim.fn.escape(word, "\\/") .. "\\>")
+      end)
+    )
   end,
 })
