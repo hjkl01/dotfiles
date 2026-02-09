e85f8bdd49c67cc75065b65b09129b291d2ef00a
ycm76229 <ycm76229@gmail.com>
2026-02-09 11:18:47 +0800

    添加Nvim判断当前是否是SSH远程

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,14 +5,17 @@
 vim.opt.wrap = true
 vim.opt.clipboard = "unnamedplus"
 
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+-- 如果是 SSH 远程会话，使用 OSC 52 协议
+if os.getenv("SSH_CONNECTION") or os.getenv("SSH_CLIENT") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
