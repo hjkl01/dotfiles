1cbedbd0d61b02ed1e81dbffff8d9eb3bdf440ac
hjkl01 <ycm76229@gmail.com>
2026-03-11 09:40:55 +0800

    update zsh config

--- zsh/asdf.sh
+++ zsh/asdf.sh
@@ -5,16 +5,16 @@
 
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
-asdf_completions_path=${ASDF_DATA_DIR:-$HOME/.asdf}/completions
-if [ -f $asdf_completions_path ]; then
-	mkdir -p $asdf_completions_path
-  asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
-fi
+# asdf_completions_path=${ASDF_DATA_DIR:-$HOME/.asdf}/completions
+# if [ -f $asdf_completions_path ]; then
+# 	mkdir -p $asdf_completions_path
+#   asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
+# fi
 
 # 添加补全功能到 fpath
-fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
+# fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
 # 使用 ZSH 的 compinit 初始化补全功能
-autoload -Uz compinit && compinit
+# autoload -Uz compinit && compinit
 
 # rust
 export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
