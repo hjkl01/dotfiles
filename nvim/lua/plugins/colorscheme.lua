991f7f4e74eac4cddb077bc8178edabf6b8522ab
hjkl01 <ycm76229@gmail.com>
2026-03-26 17:01:17 +0800

    设置Neovim透明背景

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,4 +9,22 @@ return {
       colorscheme = "gruvbox",
     },
   },
+
+  {
+    "LazyVim/LazyVim",
+    opts = function(_, opts)
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "gruvbox",
+        callback = function()
+          local hi = vim.api.nvim_set_hl
+          hi(0, "Normal", { bg = "none" })
+          hi(0, "NormalFloat", { bg = "none" })
+          hi(0, "LineNr", { bg = "none" })
+          hi(0, "Folded", { bg = "none" })
+          hi(0, "NonText", { bg = "none" })
+          hi(0, "EndOfBuffer", { bg = "none" })
+        end,
+      })
+    end,
+  },
 }
