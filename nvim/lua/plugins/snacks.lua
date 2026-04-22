7977c9617d48d12e5f5e3bbcb88a8a245eddeb49
hjkl01 <ycm76229@gmail.com>
2026-04-22 14:35:42 +0800

    update snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,8 +2,18 @@ local M = {}
 
 function M.setup()
   require("snacks").setup({
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
   })
 end
 
