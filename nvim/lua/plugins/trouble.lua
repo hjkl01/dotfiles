3a60c92b0fb48ffca8a7b6a31fb1803a9e30528a
hjkl01 <ycm76229@gmail.com>
2026-04-22 11:37:17 +0800

    update trouble: 默认显示diagnostics信息

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -5,6 +5,29 @@ function M.setup()
 
   vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
   vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix (Trouble)" })
+
+
+  local last_line = -1
+
+  vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+      local line = vim.api.nvim_win_get_cursor(0)[1]
+      if line == last_line then return end
+      last_line = line
+
+      local diagnostics = vim.diagnostic.get(0, { lnum = line - 1 })
+
+      for _, d in ipairs(diagnostics) do
+        if d.severity == vim.diagnostic.severity.WARN then
+          vim.diagnostic.open_float(nil, {
+            focus = false,
+            scope = "cursor",
+          })
+          return
+        end
+      end
+    end,
+  })
 end
 
 return M
